1cd3b8e9a02bc4814337c1b39c7fb9244f75ea78
Travis Veazey <travisvz@gmail.com>
2021-08-19 11:37:27 -0800

    Turret now a cylinder; bullets emit light

--- tanks/Bullet.gd
+++ tanks/Bullet.gd
@@ -1,14 +1,13 @@
 extends Spatial
 
-export var BULLET_VELOCITY = Vector3.FORWARD
+export var BULLET_SPEED = 12.0
 export var BULLET_TIME = 4.0
+export var BULLET_DAMAGE = 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var timer = get_tree().create_timer(BULLET_TIME)
 	timer.connect("timeout", self, "queue_free")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	global_translate(transform.basis.z * -BULLET_SPEED * delta)

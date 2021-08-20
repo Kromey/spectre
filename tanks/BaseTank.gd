34fe59943a1b503ebb56b682ee8a2aeea601353b
Travis Veazey <travisvz@gmail.com>
2021-08-20 14:21:19 -0800

    Move tank control functions to BaseTank
    PlayerTank now merely holds input handling logic

--- tanks/BaseTank.gd
+++ tanks/BaseTank.gd
@@ -4,6 +4,22 @@ var falling = 0.0
 const GRAVITY = 2
 var damage = 0
 export(int) var MAX_DAMAGE = 1
+var velocity = Vector3.ZERO
+export(float) var MAX_SPEED = 10
+export(float) var ACCEL = 2
+
+var angular_velocity = 0
+export(float) var TURN_RATE = PI / 2.0
+export(float) var TURN_ACCEL = 8
+
+enum Direction {
+	FORWARD = -1,
+	BACK = 1,
+	LEFT = 1,
+	RIGHT = -1
+}
+
+const Bullet = preload("res://tanks/Bullet.tscn")
 
 signal dead
 
@@ -19,6 +35,48 @@ func _physics_process(delta):
 	
 	if translation.y < -5:
 		take_damage(Vector3.DOWN, MAX_DAMAGE * MAX_DAMAGE)
+
+func drive(direction, delta):
+	if is_on_floor():
+		var hvel = velocity
+		hvel.y = 0
+		
+		var dir = transform.basis.z * clamp(direction, -1.0, 1.0)
+		
+		var target = dir * MAX_SPEED
+		
+		var accel = ACCEL
+		if dir.dot(hvel) <= 0:
+			accel *= 2
+		
+		hvel = hvel.linear_interpolate(target, accel * delta)
+		velocity.x = hvel.x
+		velocity.z = hvel.z
+	
+	velocity = move_and_slide(velocity, Vector3.UP)
+
+func turn(direction, delta):
+	if is_on_floor():
+		var turn_input = direction * TURN_RATE * delta
+		
+		var accel = TURN_ACCEL * delta
+		if abs(turn_input) < abs(angular_velocity):
+			# Stop turning twice as fast as we started
+			accel *= 2
+		
+		angular_velocity = lerp(angular_velocity, turn_input, accel)
+	
+	rotate_y(angular_velocity)
+
+func shoot(speed, gun_range, damage = 1):
+	var bullet = Bullet.instance()
+	bullet.BULLET_TIME = gun_range / speed
+	bullet.BULLET_SPEED = speed
+	bullet.BULLET_DAMAGE = damage
+	bullet.global_transform = $BulletSpawn.global_transform
+	get_tree().root.add_child(bullet)
+	
+	$MuzzleFlare.restart()
 
 func take_damage(_force, amount):
 	damage += amount
--- tanks/PlayerTank.gd
+++ tanks/PlayerTank.gd
@@ -1,17 +1,5 @@
 extends "res://tanks/BaseTank.gd"
 
-#const GRAVITY = 2 * Vector3.DOWN
-const ACCEL = 2
-const DEACCEL = ACCEL * 2
-const MAX_SPEED = 10
-const TURN_RATE = PI / 2.0 # Radians/sec
-const TURN_INERTIA = 8
-
-var Bullet = preload("res://tanks/Bullet.tscn")
-
-var velocity = Vector3.ZERO
-var turn = 0
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -22,58 +10,23 @@ func _physics_process(delta):
 	var turn_input = 0
 	
 	if Input.is_action_pressed("movement_left"):
-		turn_input += TURN_RATE * delta
+		turn_input += Direction.LEFT
 	if Input.is_action_pressed("movement_right"):
-		turn_input -= TURN_RATE * delta
-	
-	var inertia = TURN_INERTIA
-	if abs(turn_input) < abs(turn):
-		# Stop turning twice as fast as we started
-		inertia *= 2
-	
-	turn = lerp(turn, turn_input, inertia * delta)
-	rotate_y(turn)
+		turn_input += Direction.RIGHT
+	turn(turn_input, delta)
 	# -----
-	
-	# -----
-	# Gravity
-	#velocity += GRAVITY * delta
 	
 	# -----
 	# Acceleration
+	var dir = 0
 	if is_on_floor():
-		var dir = Vector3.ZERO
-		var hvel = velocity
-		hvel.y = 0
-		
 		if Input.is_action_pressed("movement_forward"):
-			dir += Vector3.FORWARD
+			dir += Direction.FORWARD
 		if Input.is_action_pressed("movement_backward"):
-			dir += Vector3.BACK
-		
-		dir = transform.basis.z * dir.normalized().z
-		
-		var target = dir * MAX_SPEED
-		
-		var accel
-		if dir.dot(hvel) > 0:
-			accel = ACCEL
-		else:
-			accel = DEACCEL
-		
-		hvel = hvel.linear_interpolate(target, accel * delta)
-		velocity.x = hvel.x
-		velocity.z = hvel.z
-	
-	velocity = move_and_slide(velocity, Vector3.UP)
+			dir += Direction.BACK
+	drive(dir, delta)
 	# -----
 
 func _input(event):
 	if event.is_action_pressed("action_shoot"):
-		var bullet = Bullet.instance()
-		#bullet.BULLET_TIME = 1.5
-		bullet.BULLET_SPEED = MAX_SPEED * 1.2
-		bullet.global_transform = $BulletSpawn.global_transform
-		get_tree().root.add_child(bullet)
-		
-		$MuzzleFlare.restart()
+		shoot(MAX_SPEED * 1.2, 50)

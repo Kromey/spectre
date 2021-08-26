b034b8f3ec681e473f729da6f9537a9b2e87480b
Travis Veazey <travisvz@gmail.com>
2021-08-26 14:06:11 -0800

    New, improved model for armor pickups
    Looks better and now oriented to be seen more easily

--- ArmorPickup.gd
+++ ArmorPickup.gd
@@ -4,7 +4,7 @@ export(int) var pickup_value = 2
 
 func _ready():
 	$SpinnyAnim.play("Spinny")
-	$PulsyAnim.play("Pulsy")
+	#$PulsyAnim.play("Pulsy")
 
 func _on_Area_body_entered(body):
 	if body.is_in_group("player"):

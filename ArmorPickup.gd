f49603e812b0feb334eb2bbe50a8fccb930a6814
Travis Veazey <travisvz@gmail.com>
2021-08-26 15:01:37 -0800

    Use a Tween to animate pickups
    This allows the initial rotation to be randomized, which allows pickups spawned at the same time to be out of sync, matching behavior of pickups spawned later in a game

--- ArmorPickup.gd
+++ ArmorPickup.gd
@@ -3,8 +3,11 @@ extends Spatial
 export(int) var pickup_value = 2
 
 func _ready():
-	$SpinnyAnim.play("Spinny")
-	#$PulsyAnim.play("Pulsy")
+	rotate_y(randf() * 2 * PI)
+	
+	$Tween.interpolate_property(self, "rotation", rotation, rotation + Vector3.UP * 2 * PI, 4)
+	$Tween.repeat = true
+	$Tween.start()
 
 func _on_Area_body_entered(body):
 	if body.is_in_group("player"):

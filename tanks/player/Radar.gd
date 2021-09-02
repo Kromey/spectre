24c61c1d646499287d6aa940880342ac1053237d
Travis Veazey <travisvz@gmail.com>
2021-09-02 12:30:51 -0800

    More radar icons
    Pickups are not working due to (for some reason) the Main script not
    being able to add them directly, and instead having to defer them to a
    subsequent frame, resulting in them not being present in the scene when
    the radar is initialized.

--- tanks/player/Radar.gd
+++ tanks/player/Radar.gd
@@ -4,8 +4,12 @@ var player
 
 const Icon = preload("res://tanks/player/radar/RadarIcon.tscn")
 const EnemyIcon = preload("res://tanks/player/radar/EnemyIcon.tscn")
+const FeatureIcon = preload("res://tanks/player/radar/FeatureIcon.tscn")
+const PickupIcon = preload("res://tanks/player/radar/PickupIcon.tscn")
+const FlagIcon = preload("res://tanks/player/radar/FlagIcon.tscn")
 
 var zoom = 1.5
+var center := Vector2.ZERO
 
 func _ready():
 	set_physics_process(false)
@@ -20,18 +24,30 @@ func initialize():
 		call_deferred("initialize")
 		return
 	
-	var icon = Icon.instance()
-	icon.me = get_tree().get_nodes_in_group("zoomzoom")[0]
-	icon.center = Vector2(120, 120)
-	add_child(icon)
+	center = rect_size / 2.0
+	print(rect_size, center)
+	
+	# NB: Order matters! First icons added will be underneath later icons
+	
+	for feature in get_tree().get_nodes_in_group("map_features"):
+		add_icon(FeatureIcon, feature)
+	
+	for pickup in get_tree().get_nodes_in_group("pickups"):
+		add_icon(PickupIcon, pickup)
+	
+	for flag in get_tree().get_nodes_in_group("goals"):
+		add_icon(FlagIcon, flag)
 	
 	for enemy in get_tree().get_nodes_in_group("enemies"):
-		var enemy_icon = EnemyIcon.instance()
-		enemy_icon.me = enemy
-		enemy_icon.center = Vector2(120, 120)
-		add_child(enemy_icon)
+		add_icon(EnemyIcon, enemy)
 	
 	set_physics_process(true)
+
+func add_icon(packed_icon: PackedScene, node: Spatial):
+	var icon = packed_icon.instance()
+	icon.me = node
+	icon.center = center
+	add_child(icon)
 
 func _physics_process(_delta):
 	get_tree().call_group("radar_icons", "update_pos", player.global_transform, zoom)

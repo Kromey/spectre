901b430b255af3073837ceda3e9a3ab9ce3fde1d
Travis Veazey <travisvz@gmail.com>
2021-09-22 10:30:25 -0800

    Smoother credits crawl, multiple clicks no longer stack them

---
+++ main_menu/Credits.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+const CREDITS := [
+	["Game Design & Programming", "Travis Veazey",],
+	["3D Models & Textures", "Travis Veazey",],
+]
+const CREDITS_LINE_TIME := 0.3
+
+onready var line_timer = $LineTimer
+var is_rolling := false
+
+func roll():
+	is_rolling = !is_rolling
+	get_tree().call_group("running_credits", "queue_free")
+	
+	var label = $CreditsLabel
+	var title: bool
+	
+	for section in CREDITS:
+		if !is_rolling:
+			return
+		
+		title = true
+		
+		for line in section:
+			if !is_rolling:
+				return
+			
+			var l = label.duplicate()
+			add_child(l)
+			l.start(line, title)
+
+			title = false
+			
+			line_timer.start()
+			yield(line_timer, "timeout")
+		
+		line_timer.start()
+		yield(line_timer, "timeout")
---
+++ main_menu/CreditsLabel.gd
@@ -0,0 +1,39 @@
+extends Label
+
+const CREDITS_LINE_TIME := 0.3
+const CREDITS_TITLE_COLOR := Color.blueviolet
+const CREDITS_SPEED := 125
+
+var fading = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	hide()
+	set_physics_process(false)
+
+func _physics_process(delta):
+	rect_global_position.y -= round(CREDITS_SPEED * delta)
+	
+	if rect_global_position.y <= 0:
+		queue_free()
+	elif rect_global_position.y < CREDITS_SPEED and !fading:
+		fading = true
+		fade_out()
+
+func start(line: String, is_title := false):
+	rect_global_position.x = 25
+	rect_global_position.y = OS.window_size.y
+	text = line
+	show()
+	add_to_group("running_credits")
+	if is_title:
+		add_color_override("font_color", CREDITS_TITLE_COLOR)
+	
+	show()
+	set_physics_process(true)
+
+func fade_out():
+	var tween = Tween.new()
+	tween.interpolate_property(self, "modulate", modulate, Color(1, 1, 1, 0), 0.5)
+	add_child(tween)
+	tween.start()

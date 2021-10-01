cf9e35a2a4b5011bd840c45dc5a6ccc91b7ea7e0
Travis Veazey <travisvz@gmail.com>
2021-10-01 14:07:45 -0800

    Console can be closed with same key that opens it

--- Console.gd
+++ Console.gd
@@ -12,7 +12,7 @@ func _ready():
 
 
 func _input(event):
-	if event.is_action_pressed("ui_cancel"):
+	if event.is_action_pressed("ui_cancel") or event.is_action_pressed("ui_console"):
 		queue_free()
 		get_tree().set_deferred("paused", false)
 

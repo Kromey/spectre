ba0043124ed09c4622a6eea6d9665f7de14fec08
Travis Veazey <travisvz@gmail.com>
2021-10-20 16:29:31 -0800

    Game Over screen no longer inherits from Pause

--- GameOverScreen.gd
+++ GameOverScreen.gd
@@ -1,9 +1,20 @@
-extends "res://PauseScreen.gd"
+extends Node
 
 
 func _enter_tree():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	find_node("Score").text = "Score: %s" % Game.player_score
 
-func _on_Resume_pressed():
+
+func _on_NewGame_pressed():
 	var e = get_tree().reload_current_scene()
 	assert(e == OK)
+
+
+func _on_MainMenu_pressed():
+	get_tree().paused = false
+	Game.set_state(Game.State.MainMenu)
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
